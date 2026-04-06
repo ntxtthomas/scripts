7bd38c361ca6bb590626b954916ec9415808261c
ntxtthomas <apm.tthomas@gmail.com>
2026-04-06 09:16:50 -0500

    reorged, renamed

--- challenges/ruby/react-button-toggle.rb
+++
@@ -1,6 +0,0 @@
-# Title: React Button Toggle
-# Difficulty: Easy
-# Solutions: 89954
-# Maximum Score: 10
-# Description: For this challenge you will be editing a small React application that implements a toggle.
-
